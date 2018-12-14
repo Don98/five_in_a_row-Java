9b0d581112eaea37c5b952ed9b630112653736a6
Don <ZHONGDAchenqd6@163.com>
2018-12-14 11:34:07 +0800

    first

---
+++ Qipan.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class Qipan extends JFrame
+{
+	
+	public Qipan()
+	{
+		super("陈钦德五子棋v1.0");
+	}
+	
+	public void init()
+	{
+		
+	}
+	
+	public void addListener()
+	{
+		
+	}
+	public static void mian(String[] args)
+	{
+		
+	}
+}
