1190b467ddbfbf62e224215da7dc80938b121625
Don <ZHONGDAchenqd6@163.com>
2018-12-15 15:49:00 +0800

    2018-12-15/15:48

--- Qipan.java
+++ Qipan.java
@@ -1,3 +1,4 @@
+import javafx.application.Application;
 import javax.swing.JFrame;
 
 public class Qipan extends JFrame
@@ -7,7 +8,7 @@ public class Qipan extends JFrame
 	{
 		super("陈钦德五子棋v1.0");
 		this.setSize(600,900);
-		this.setDefaultCloseOperation(JFram.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
 	
@@ -20,7 +21,7 @@ public class Qipan extends JFrame
 	{
 		
 	}
-	public static void mian(String[] args)
+	public static void main(String[] args)
 	{
 		new Qipan();
 	}
