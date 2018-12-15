4b024ca450ac80af7e47b3c026136aebf237ee35
Don <ZHONGDAchenqd6@163.com>
2018-12-15 16:47:39 +0800

    2018-12-15/16:47

--- Draw.java
+++ Draw.java
@@ -1,23 +1,33 @@
 import javax.swing.JPanel;
+import java.awt.*;
 
-public class Dram extends JPanel
+public class Draw extends JPanel
 {
+	private int shamt_x = 80;
+	private int shamt_y = 50;
+	
 	public Draw()
 	{
-		this.setSize(500,500);
+		this.setSize(800,800);
+		// this.setBackground(new Color(220,191,157)); // 木色
+		this.setBackground(new Color(192,192,192)); // 亮色
 	}
 	
 	@Override
 	public void paint(Graphics g)
 	{
 		super.paint(g);
+		paintQipan(g);
 	}
 	
 	public void paintQipan(Graphics g)
 	{
-		for(int i = 0;i < 15;i++)
+		int size = 15;
+		int length = 50;
+		for(int i = 0;i < size;i++)
 		{
-			g,drawLine()
+			g.drawLine(shamt_x + 0,shamt_y + i * length,shamt_x + (size - 1) * length,shamt_y + i * length);
+			g.drawLine(shamt_x + i * length ,shamt_y + 0 ,shamt_x + i * length,shamt_y + (size - 1) * length);
 		}
 	}
 	
--- Qipan.java
+++ Qipan.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JLabel;
 
 public class Qipan extends JFrame
 {
@@ -13,19 +14,24 @@ public class Qipan extends JFrame
 	private JMenu renjiduiyi;
 	private JMenuItem renzhihei;
 	private JMenuItem renzhibai;
-	
+	private Draw draw;
+	private JLabel userInfo;
+	private JLabel gameInfo;
 	
 	
 	public Qipan()
 	{
 		super("陈钦德五子棋v1.0");
-		this.setSize(600,600);
+		
+		init();
+		
+		this.setSize(900,900);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 		int width = (int)d.getWidth();
 		int hegiht = (int)d.getHeight();
 		
-		this.setLocation((width - 600)/2,(hegiht - 600)/2);
+		this.setLocation((width - 900)/2,(hegiht - 900)/2);
 		this.setVisible(true);
 	}
 	
@@ -37,6 +43,11 @@ public class Qipan extends JFrame
 		renjiduiyi = new JMenu("人机对弈");
 		renzhihei = new JMenuItem("人执黑棋");
 		renzhibai = new JMenuItem("人执白棋");
+		draw = new Draw();
+		userInfo = new JLabel("未登录");
+		userInfo.setHorizontalAlignment(JLabel.CENTER);
+		gameInfo = new JLabel("游戏未开始");
+		gameInfo.setHorizontalAlignment(JLabel.CENTER);
 		
 		bar.add(xitong);
 		xitong.add(renrenduiyi);
@@ -45,7 +56,12 @@ public class Qipan extends JFrame
 		renjiduiyi.add(renzhihei);
 		renjiduiyi.add(renzhibai);
 		
+		this.setLayout(new BorderLayout());
+		
 		this.setJMenuBar(bar);
+		this.add(userInfo,BorderLayout.NORTH);
+		this.add(draw,BorderLayout.CENTER);
+		this.add(gameInfo,BorderLayout.SOUTH);
 		
 	}
 	
