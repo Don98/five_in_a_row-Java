9f263998394e19c9cfbae3c75500a827593dcba0
Don98 <ZHONGDAchenqd6@163.com>
2018-12-15 15:43:37 +0800

    try

--- Qipan.java
+++ Qipan.java
@@ -6,6 +6,9 @@ public class Qipan extends JFrame
 	public Qipan()
 	{
 		super("陈钦德五子棋v1.0");
+		this.setSize(600,900);
+		this.setDefaultCloseOperation(JFram.EXIT_ON_CLOSE);
+		this.setVisible(true);
 	}
 	
 	public void init()
@@ -19,6 +22,6 @@ public class Qipan extends JFrame
 	}
 	public static void mian(String[] args)
 	{
-		
+		new Qipan();
 	}
 }
